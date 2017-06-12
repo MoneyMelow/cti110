2d19fc950ed0dc376620d0e5314307b0e86179a6
MoneyMelow <MoneyMelow@users.noreply.github.com>
2017-06-12 13:55:56 -0400

    Add files via upload
    6/12 Distance Traveled Prog

---
+++ M2HW1_DistanceTraveled_MarquisMarshall.py
@@ -0,0 +1,10 @@
+#DistanceTraveledProgram
+#6/6/2017
+#CTI-110 M2HW1- Distance Traveled
+#Marquis Marshall
+#
+
+speed = 70
+print ( 'In 6 hours, the car will travel ' + str( (speed * 6) ) + ' miles' )
+print ( 'In 10 hours, the car will travel ' + str( (speed * 10) ) + ' miles' )
+print ( 'In 15 hours, the car will travel ' + str( (speed * 15) ) + ' miles' )
